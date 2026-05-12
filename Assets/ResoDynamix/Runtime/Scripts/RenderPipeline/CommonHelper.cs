da5b1c885e31497049a3d2d64389903a34b9c2da
zhang_yubing <zhang_yubing@cyberagent.co.jp>
2026-05-12 13:05:21 +0900

    AddBlitPassCustomをよりシンプルな形に

--- Assets/ResoDynamix/Runtime/Scripts/RenderPipeline/CommonHelper.cs
+++ Assets/ResoDynamix/Runtime/Scripts/RenderPipeline/CommonHelper.cs
@@ -14,30 +14,23 @@ namespace ResoDynamix.Runtime.Scripts.RenderPipeline
             public bool Bilinear;
         }
 
-        public static IRasterRenderGraphBuilder AddBlitPassCustom(this RenderGraph renderGraph, 
+        public static void AddBlitPassCustom(this RenderGraph renderGraph, 
             in TextureHandle source, in TextureHandle destination, in Vector2 scale, in Vector2 bias, 
-            in int mipLevel = 0, in bool bilinear = true, in bool returnBuilder = false,
-            in string passName = "CustomBlitPass")
+            in int mipLevel = 0, in bool bilinear = true, in string passName = "CustomBlitPass")
         {
-            var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData);
-            passData.Source = source;
-            passData.ScaleBias.Set(scale.x, scale.y, bias.x, bias.y);
-            passData.MipLevel = mipLevel;
-            passData.Bilinear = bilinear;
-            builder.SetRenderAttachment(destination, 0, AccessFlags.WriteAll);
-            builder.UseTexture(passData.Source);
-            builder.SetRenderFunc(static (PassData data, RasterGraphContext ctx) =>
+            using (var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData))
             {
-                Blitter.BlitTexture(ctx.cmd, data.Source, data.ScaleBias, data.MipLevel, data.Bilinear);
-            });
-            
-            if (returnBuilder)
-            {
-                return builder;
+                passData.Source = source;
+                passData.ScaleBias.Set(scale.x, scale.y, bias.x, bias.y);
+                passData.MipLevel = mipLevel;
+                passData.Bilinear = bilinear;
+                builder.SetRenderAttachment(destination, 0, AccessFlags.WriteAll);
+                builder.UseTexture(passData.Source);
+                builder.SetRenderFunc(static (PassData data, RasterGraphContext ctx) =>
+                {
+                    Blitter.BlitTexture(ctx.cmd, data.Source, data.ScaleBias, data.MipLevel, data.Bilinear);
+                });
             }
-            
-            builder.Dispose();
-            return null;
         }
     }
 }
